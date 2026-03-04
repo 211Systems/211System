94268810bffbff23edbefea743cc70c2949ed0a9
RadoslawJan <radek7845@gmail.com>
2026-03-04 15:30:38 +0100

    Dodanie klas dla szpitala

---
+++ 211system/Models/Hospital/Hospital.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _211system.Models.Hospital
+{
+    public class Hospital
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        public bool HasSOR { get; set; } = false;
+        [Required]
+        [MaxLength(150)]
+        public string Address { get; set; }
+
+        public virtual ICollection<Paramedic> Paramedics { get; set; } = new List<Paramedic>();
+        public virtual ICollection<HospitalWard> Wards { get; set; } = new List<HospitalWard>();
+        public virtual ICollection<Ambulance> Ambulances { get; set; } = new List<Ambulance>();
+
+    }
+}
