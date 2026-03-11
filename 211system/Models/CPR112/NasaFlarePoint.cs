6d25999ec275119326fd24d02652b5e39e7fea45
Kamil <kamilem.kuba@gmail.com>
2026-03-11 19:26:18 +0100

    Added Nasa

--- 211system/Models/CPR112/NasaFlarePoint.cs
+++ 211system/Models/CPR112/NasaFlarePoint.cs
@@ -7,5 +7,7 @@ public class NasaFlarePoint
     [Key]
     public Guid Id { get; set; }
     public double Brightness { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
     public DateTime DetectionDate { get; set; }
 }
