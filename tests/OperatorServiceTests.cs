4e4aa93c21203d3a1579834fffe63d5d801472b7
Błażej Głowacki <blazej.glowacki@wp.pl>
2026-03-25 20:19:32 +0100

    #63

--- tests/OperatorServiceTests.cs
+++ tests/OperatorServiceTests.cs
@@ -8,6 +8,8 @@ using _211system.DTOs;
 using _211system.Models.Interfaces;
 using _211system.Services;
 using CPR112.Models;
+using _211system.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace _211system.Tests
 {
@@ -21,19 +23,27 @@ namespace _211system.Tests
             return new _211DbContext(options);
         }
 
+        private Mock<UserManager<ApplicationUser>> GetUserManagerMock()
+        {
+            var store = new Mock<IUserStore<ApplicationUser>>();
+            return new Mock<UserManager<ApplicationUser>>(
+                store.Object, null, null, null, null, null, null, null, null);
+        }
+
         [Fact]
         public async Task CreateAsync_WhenEncExists_ShouldAddOperatorAndAccount()
         {
             var context = GetInMemoryDbContext();
-            
             var authMock = new Mock<IAuthService>();
+            var userManagerMock = GetUserManagerMock();
+            
             var fakeAccountId = "test-identity-id";
             var fakePassword = "TempPassword123";
             
-            authMock.Setup(a => a.CreateTemporaryAccountAsync(It.IsAny<string>(), "Dyspozytor112"))
+            authMock.Setup(a => a.CreateTemporaryAccountAsync(It.IsAny<string>(), It.IsAny<string>()))
                     .ReturnsAsync((fakeAccountId, fakePassword));
 
-            var operatorService = new OperatorService(context, authMock.Object);
+            var operatorService = new OperatorService(context, authMock.Object, userManagerMock.Object);
             
             var validEncId = Guid.NewGuid();
             context.Encs.Add(new Enc { Id = validEncId, Name = "CPR Warszawa", Region = "Mazowieckie" });
@@ -44,9 +54,11 @@ namespace _211system.Tests
                 FirstName = "Anna",
                 LastName = "Nowak",
                 Email = "anna@112.pl",
+                Rank = "Dyspozytor112",
                 StationNumber = "Stanowisko-5",
                 EncId = validEncId
             };
+
 
             var (result, tempPassword) = await operatorService.CreateAsync(createDto);
 
