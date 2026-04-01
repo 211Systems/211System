87f833802c79d3a2c91ddd4191a7c3918b81f3da
Kamil <78047380+kamilekbeznosa@users.noreply.github.com>
2026-04-01 17:38:50 +0200

    changed tokens, and added SAS tokens (#74)

--- 211system/Models/Interfaces/IBlobStorageService.cs
+++ 211system/Models/Interfaces/IBlobStorageService.cs
@@ -4,5 +4,6 @@
     {
         Task<string> UploadAsync(IFormFile file, string containerName);
         Task<bool> DeleteAsync(string fileUrl, string containerName);
+        string GetSecureFileUrl(string fileUrl, string containerName, int expireMinutes = 15);
     }
 }
