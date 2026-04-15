2e13a7c7314fce72eb0d8a751d4456c65b983366
RadoslawJan <radek7845@gmail.com>
2026-04-15 09:28:58 +0200

    FD and PS changes

---
+++ 211system/Areas/Police/Controllers/OperationsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _211system.Areas.Police.Controllers
+{
+    [Area("Police")]
+    public class OperationsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
