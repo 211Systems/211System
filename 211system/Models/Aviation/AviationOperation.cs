e0169e0c7c82305953622234c99f9985f754b988
Kamil <kamilem.kuba@gmail.com>
2026-05-19 23:15:52 +0200

    poprawione aviations

---
+++ 211system/Models/Aviation/AviationOperation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using CPR112.Models;
+
+namespace _211system.Models.Aviation
+{
+    public class AviationOperation
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        public Guid AirUnitId { get; set; }
+        [ForeignKey("AirUnitId")]
+        public AirUnit AirUnit { get; set; }
+
+        public Guid? IncidentId { get; set; }
+        [ForeignKey("IncidentId")]
+        public Incident Incident { get; set; }
+
+        [Required]
+        public DateTime StartTime { get; set; }
+
+        public DateTime? EndTime { get; set; }
+    }
+}
